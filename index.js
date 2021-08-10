374c48498983d4f5b0807b0afbb6f6cab0ef5761
Mitchell <mitchellmujwit@protonmail.com>
2021-08-10 11:36:46 -0400

    herkou

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fetch = require('node-fetch') 
 const { Client, Intents } = require('discord.js')
-const { token } = require('./config.json')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-client.login(token)
+client.login(process.env.token)
 
 client.once('ready', () => {
 	console.log('Ready!');
